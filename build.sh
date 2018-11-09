ab53a93d85dc3a8dcabed6c7342162ee53dc308f
Franc[e]sco <lolisamurai@tfwno.gf>
2018-11-09 01:08:42 +0100

    build.sh: clear logcat

--- build.sh
+++ build.sh
@@ -137,6 +137,7 @@ EOF
   adb shell pm clear $namespace || return $?
   adb uninstall $namespace || return $?
   adb install "bin/${exename}.apk" || return $?
+  adb logcat -c
   adb shell am start -n $namespace/.MainActivity || return $?
   adb logcat | grep "^./$namespace\..*$" || return $?
 }
